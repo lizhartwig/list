688ebe50e9f73a01388b91a02aa0a675f4527402
Elizabeth Hartwig <lizhartwig@yahoo.com>
2015-10-09 17:39:07 -0700

    add input for for user to add their name

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
 	$("#blanks form").submit(function(event) {
+		var nameInput = $("input#name").val();
 		var favoritething1Input = $("input#favoritething1").val();
 		var favoritething2Input = $("input#favoritething2").val();
 		var favoritething3Input = $("input#favoritething3").val();
 
+		$(".name").text(nameInput);
 		$(".favoritething1").text(favoritething1Input);
 		$(".favoritething2").text(favoritething2Input);
 		$(".favoritething3").text(favoritething3Input);
